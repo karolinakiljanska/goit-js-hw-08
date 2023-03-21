15bc70b55d932d065fe26ac007c7d575a199d65f
karolinakiljanska <karolinakiljanska112@gmail.com>
2023-03-21 21:27:35 +0100

    zadanie 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,3 +3,38 @@ import '../css/03-feedback.css';
 import throttle from 'lodash.throttle';
 
 const STORAGE_kEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+
+initialForm();
+
+form.addEventListener('input', throttle(onInput, 500));
+
+function onInput() {
+  const formData = new FormData(form);
+  let userForm = {};
+  formData.forEach((value, name) => (userForm[name] = value));
+  localStorage.setItem(localStorageKey, JSON.stringify(userForm));
+}
+
+function initialForm() {
+  let persistedForm = localStorage.getItem('localStorageKey');
+  if (persistedForm) {
+    persistedForm = JSON.parse(persistedForm);
+    console.log(persistedForm);
+    Object.entries(persistedForm).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
+  }
+}
+
+form.addEventListener('submit', onSubmit);
+function onSubmit(evt) {
+  evt.preventDefault();
+  localStorage.removeItem(localStorageKey);
+  let userForm = {};
+  const formData = new FormData(form);
+  formData.forEach((value, name) => (userForm[name] = value));
+  console.log(userForm);
+  form.reset();
+}
